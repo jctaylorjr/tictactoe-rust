e15926a23a78f1ae80e4140df568f9faf9707dd3
Jefferson Taylor <jtay@Jeffersons-MacBook-Pro.local>
2026-03-13 15:56:43 -0400

    win condition checked

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,10 @@
 use std::error::Error;
 use std::fmt::Display;
+// use itertools::Itertools;
 
 const BOARD_SIZE: usize = 9;
 
-#[derive(Clone, Copy, Debug)]
+#[derive(Clone, Copy, Debug, PartialEq)]
 pub enum TileState {
     Empty,
     X,
@@ -22,7 +23,7 @@ impl TileState {
 
 #[derive(Debug)]
 enum GameError {
-    InvalidTileSelection,
+    TileOccupied,
 }
 
 impl Error for GameError {}
@@ -30,7 +31,7 @@ impl Error for GameError {}
 impl Display for GameError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            GameError::InvalidTileSelection => write!(f, "Invalid tile selection!"),
+            GameError::TileOccupied => write!(f, "Tile already occupied!"),
         }
     }
 }
@@ -53,7 +54,7 @@ impl Tile {
                 self.state = state;
                 Ok(())
             }
-            _ => Err(GameError::InvalidTileSelection),
+            _ => Err(GameError::TileOccupied),
         }
     }
 
@@ -65,16 +66,26 @@ impl Tile {
 #[derive(Clone)]
 pub struct Player {
     pub team: TileState,
+    pub magic_square_values: Vec<i32>,
 }
 
 impl Player {
     pub fn new(team: TileState) -> Player {
-        Player { team }
+        Player { team, magic_square_values: vec![] }
     }
 
     pub fn player_team(&self) -> &str {
         self.team.enum_str()
     }
+
+    // pub fn check_win_condition(&self) -> bool {
+    //     // Check if the player's magic square values sum to 15 for any combination of three tiles
+    //     if self.magic_square_values.len() < 3 {
+    //         return false;
+    //     }
+
+    //     self.magic_square_values.iter().combinations(3).any(|combo| combo.iter().sum::<i32>() == 15)
+    // }
 }
 
 pub struct Game {
@@ -101,9 +112,16 @@ impl Game {
                 "Player {}'s turn! Select a tile (1-{}): ",
                 current_player.player_team(), BOARD_SIZE
             );
-            // println!("{tile_choice}")
-            while let Err(_) = self.board[Self::input_selection()].update_state(current_player.team) {
-                println!("Tile already occupied! Please select another tile: ");
+
+            let selection = Self::input_selection();
+            while let Err(e) = self.board[selection].update_state(current_player.team) {
+                println!("{e} Please select another tile: ");
+            }
+        
+            // TODO: Check for win condition after each turn
+            if self.check_win_condition() {
+                println!("Player {} wins!", current_player.player_team());
+                return;
             }
         }
         println!("{:?}", self.board);
@@ -124,5 +142,27 @@ impl Game {
                 println!("Invalid input! Please enter a number between 1 and {}: ", BOARD_SIZE);
             }
         }
+    }
+
+    fn check_win_condition(&self) -> bool {
+        let win_combinations = [
+            [0, 1, 2], // Row 1
+            [3, 4, 5], // Row 2
+            [6, 7, 8], // Row 3
+            [0, 3, 6], // Column 1
+            [1, 4, 7], // Column 2
+            [2, 5, 8], // Column 3
+            [0, 4, 8], // Diagonal \
+            [2, 4, 6], // Diagonal /
+        ];
+
+        for combo in &win_combinations {
+            let (a, b, c) = (self.board[combo[0]].state, self.board[combo[1]].state, self.board[combo[2]].state);
+            if a != TileState::Empty && a == b && b == c {
+                return true;
+            }
+        }
+
+        false
     }
 }

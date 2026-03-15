b6707f5241004a86d2ccf9a99fb57e9e217bcad2
Jefferson Taylor <jctaylor2nd@gmail.com>
2026-03-15 17:00:02 -0400

    added board display and colored the teams when tile selected

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,5 @@
 use std::error::Error;
 use std::fmt::Display;
-// use itertools::Itertools;
 
 const BOARD_SIZE: usize = 9;
 
@@ -66,26 +65,16 @@ impl Tile {
 #[derive(Clone)]
 pub struct Player {
     pub team: TileState,
-    pub magic_square_values: Vec<i32>,
 }
 
 impl Player {
     pub fn new(team: TileState) -> Player {
-        Player { team, magic_square_values: vec![] }
+        Player { team }
     }
 
     pub fn player_team(&self) -> &str {
         self.team.enum_str()
     }
-
-    // pub fn check_win_condition(&self) -> bool {
-    //     // Check if the player's magic square values sum to 15 for any combination of three tiles
-    //     if self.magic_square_values.len() < 3 {
-    //         return false;
-    //     }
-
-    //     self.magic_square_values.iter().combinations(3).any(|combo| combo.iter().sum::<i32>() == 15)
-    // }
 }
 
 pub struct Game {
@@ -97,34 +86,34 @@ impl Game {
     pub fn new() -> Game {
         Game {
             board: vec![Tile::new(); BOARD_SIZE],
-            players: vec![
-                Player::new(TileState::X),
-                Player::new(TileState::O),
-            ],
+            players: vec![Player::new(TileState::X), Player::new(TileState::O)],
         }
     }
 
     pub fn start_game(&mut self) {
+        self.print_board();
         let mut player_iter = self.players.iter().cycle();
         for _ in 0..BOARD_SIZE {
             let current_player = player_iter.next().unwrap();
             println!(
                 "Player {}'s turn! Select a tile (1-{}): ",
-                current_player.player_team(), BOARD_SIZE
+                current_player.player_team(),
+                BOARD_SIZE
             );
 
-            let selection = Self::input_selection();
-            while let Err(e) = self.board[selection].update_state(current_player.team) {
+            while let Err(e) = self.board[Self::input_selection()].update_state(current_player.team)
+            {
                 println!("{e} Please select another tile: ");
             }
-        
-            // TODO: Check for win condition after each turn
+
+            self.print_board();
+
             if self.check_win_condition() {
                 println!("Player {} wins!", current_player.player_team());
                 return;
             }
         }
-        println!("{:?}", self.board);
+        println!("It's a draw!");
     }
 
     fn input_selection() -> usize {
@@ -139,30 +128,57 @@ impl Game {
                 if (1..=BOARD_SIZE).contains(&n) {
                     return (n - 1) as usize;
                 }
-                println!("Invalid input! Please enter a number between 1 and {}: ", BOARD_SIZE);
+                println!(
+                    "Invalid input! Please enter a number between 1 and {}: ",
+                    BOARD_SIZE
+                );
             }
         }
     }
 
     fn check_win_condition(&self) -> bool {
         let win_combinations = [
-            [0, 1, 2], // Row 1
-            [3, 4, 5], // Row 2
-            [6, 7, 8], // Row 3
-            [0, 3, 6], // Column 1
-            [1, 4, 7], // Column 2
-            [2, 5, 8], // Column 3
-            [0, 4, 8], // Diagonal \
-            [2, 4, 6], // Diagonal /
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6],
         ];
 
         for combo in &win_combinations {
-            let (a, b, c) = (self.board[combo[0]].state, self.board[combo[1]].state, self.board[combo[2]].state);
+            let (a, b, c) = (
+                self.board[combo[0]].state,
+                self.board[combo[1]].state,
+                self.board[combo[2]].state,
+            );
             if a != TileState::Empty && a == b && b == c {
                 return true;
             }
         }
 
         false
+    }
+
+    fn print_board(&self) {
+        self.clear_terminal();
+        for (i, tile) in self.board.iter().enumerate() {
+            if tile.state == TileState::Empty {
+                print!(" {} ", i + 1);
+            } else if tile.state == TileState::X {
+                print!("\x1b[31m {} \x1b[0m", tile.display_char());
+            } else {
+                print!("\x1b[32m {} \x1b[0m", tile.display_char());
+            }
+            if (i + 1) % 3 == 0 {
+                println!();
+            }
+        }
+    }
+
+    fn clear_terminal(&self) {
+        print!("\x1B[2J\x1B[1;1H");
     }
 }
